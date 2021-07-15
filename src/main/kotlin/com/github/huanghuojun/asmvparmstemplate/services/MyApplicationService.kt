a6ccf73122948546dbfb8b1f6819f252c1b03906
GitHub Action <action@github.com>
2021-07-15 01:21:34 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/huanghuojun/asmvparmstemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.huanghuojun.asmvparmstemplate.services
+
+import com.github.huanghuojun.asmvparmstemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/huanghuojun/asmvparmstemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/huanghuojun/asmvparmstemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.huanghuojun.asmvparmstemplate.services
 
+import com.github.huanghuojun.asmvparmstemplate.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
